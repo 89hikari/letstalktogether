4cb9172be3405bda3d950ccca0cf6de1b06769ff
89hikari <vladislav0151@bk.ru>
2020-07-05 03:13:42 +0300

    start creating main logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className='header'>
+        <img src='https://s2.logaster.com/static/v3/img/products/logo.png' />
       </header>
+      <nav className='nav'>
+        <div>
+          <a href='#'>My Profile</a>
+        </div>
+        <div>
+        <a href='#'>Messages</a>
+        </div>
+        <div>
+        <a href='#'>News</a>
+        </div>
+        <div>
+        <a href='#'>Settings</a>
+        </div>
+      </nav>
+      <div className='contant'>
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGMf5RLsxY1VKguLz-6s6bhYUkC3xWrAmG3Q&usqp=CAU"/>
+        </div>
+        <div>
+          avatar + description
+        </div>
+        <div>
+          My Posts
+          <div>
+            New Post
+          </div>
+          <div>
+            <div>
+              Post1
+            </div>
+            <div>
+              Post2
+            </div>
+            <div>
+              Post3
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
