e36f5fedaed2730bae86a0a8e9714a584c5f4776
89hikari <vladislav0151@bk.ru>
2020-07-07 02:40:33 +0300

    profile's component

--- src/App.js
+++ src/App.js
@@ -2,37 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <div className='contant'>
-        <div>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGMf5RLsxY1VKguLz-6s6bhYUkC3xWrAmG3Q&usqp=CAU" />
-        </div>
-        <div>
-          avatar + description
-        </div>
-        <div>
-          My Posts
-          <div>
-            New Post
-          </div>
-          <div>
-            <div>
-              Post1
-            </div>
-            <div>
-              Post2
-            </div>
-            <div>
-              Post3
-            </div>
-          </div>
-        </div>
-      </div>
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='contant'>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGMf5RLsxY1VKguLz-6s6bhYUkC3xWrAmG3Q&usqp=CAU" />
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                    <div>
+                        Post3
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
