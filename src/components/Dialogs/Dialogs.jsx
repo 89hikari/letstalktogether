8a564178a97387db0389635f6a5f64dbd33679f8
89hikari <vladislav0151@bk.ru>
2020-07-12 21:26:57 +0300

    more styles

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Vlad
+                </div>
+                <div className={s.dialog}>
+                    Andryxa
+                </div>
+                <div className={s.dialog}>
+                    Vanes
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hru</div>
+                <div className={s.message}>Yo yo yo</div>
+            </div>
         </div>
     );
 }
