56d41441302acd2c4e4fe40fcc6b342fe6a1bc93
89hikari <vladislav0151@bk.ru>
2020-07-16 13:19:51 +0300

    style changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,24 +6,26 @@ import SitebarFriends from './SitebarFriends'
 const Navbar = (props) => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}>
-                <NavLink to='/profile' activeClassName={classes.active}>Profile</NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to='/dialogs' activeClassName={classes.active}>Messages</NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to='#' activeClassName={classes.active}>News</NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to='#' activeClassName={classes.active}>Music</NavLink>
-            </div>
-            <div className={classes.item}>
-                <NavLink to='#' activeClassName={classes.active}>Settings</NavLink>
+            <div className={classes.links}>
+                <div className={classes.item2 + ' ' + classes.profile}>
+                    <NavLink to='/profile' activeClassName={classes.active}><i className="fa fa-address-card-o" aria-hidden="true"></i></NavLink>
+                </div>
+                <div className={classes.item2 + ' ' + classes.messages}>
+                    <NavLink to='/dialogs' activeClassName={classes.active}><i class="fa fa-comments-o" aria-hidden="true"></i></NavLink>
+                </div>
+                <div className={classes.item2 + ' ' + classes.news}>
+                    <NavLink to='/#' activeClassName={classes.active}><i class="fa fa-newspaper-o" aria-hidden="true"></i></NavLink>
+                </div>
+                <div className={classes.item2 + ' ' + classes.music}>
+                    <NavLink to='/#' activeClassName={classes.active}><i class="fa fa-music" aria-hidden="true"></i></NavLink>
+                </div>
+                <div className={classes.item2 + ' ' + classes.settings}>
+                    <NavLink to='/#' activeClassName={classes.active}><i class="fa fa-cogs" aria-hidden="true"></i></NavLink>
+                </div>
             </div>
             <hr></hr>
-            <SitebarFriends friends={props.state}/>
-        </nav>
+            <SitebarFriends friends={props.state} />
+        </nav >
     );
 }
 
--- src/components/Navbar/SitebarFriends.jsx
+++ src/components/Navbar/SitebarFriends.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import classes from './SitebarFriends.module.css'
+import s from './SitebarFriends.module.css'
 import { NavLink } from 'react-router-dom';
 
 const SitebarFriends = (props) => {
 
-    let friendsElements = props.friends.map(m => <NavLink to='/' activeClassName={classes.active}>
-    <div className={classes.item}>
+    let friendsElements = props.friends.map(m => <NavLink to='/' activeClassName={s.active}>
+    <div className={s.item}>
         <img src={m.img} />
         <div></div>
         <span>{m.name}</span>
@@ -13,9 +13,9 @@ const SitebarFriends = (props) => {
     </NavLink>);
 
     return (
-        <div className={classes.container}>
-            <h2 className={classes.title}>Friends</h2>
-            <div className={classes.friends}>
+        <div className={s.container}>
+            <h2 className={s.title}>Friends</h2>
+            <div className={s.friends}>
                 { friendsElements }
             </div>
         </div>
