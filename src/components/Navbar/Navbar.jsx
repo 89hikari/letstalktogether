63ad2f381fcb40d1c96312dfa531840b81d22678
89hikari <vladislav0151@bk.ru>
2020-07-16 15:52:17 +0300

    adaptive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,16 +11,16 @@ const Navbar = (props) => {
                     <NavLink to='/profile' activeClassName={classes.active}><i className="fa fa-address-card-o" aria-hidden="true"></i><p className={classes.text}>Profile</p></NavLink>
                 </div>
                 <div className={classes.item2 + ' ' + classes.messages}>
-                    <NavLink to='/dialogs' activeClassName={classes.active}><i class="fa fa-comments-o" aria-hidden="true"></i><p className={classes.text}>Messages</p></NavLink>
+                    <NavLink to='/dialogs' activeClassName={classes.active}><i className="fa fa-comments-o" aria-hidden="true"></i><p className={classes.text}>Messages</p></NavLink>
                 </div>
                 <div className={classes.item2 + ' ' + classes.news}>
-                    <NavLink to='/#' activeClassName={classes.active}><i class="fa fa-newspaper-o" aria-hidden="true"></i><p className={classes.text}>News</p></NavLink>
+                    <NavLink to='/#' activeClassName={classes.active}><i className="fa fa-newspaper-o" aria-hidden="true"></i><p className={classes.text}>News</p></NavLink>
                 </div>
                 <div className={classes.item2 + ' ' + classes.music}>
-                    <NavLink to='/#' activeClassName={classes.active}><i class="fa fa-music" aria-hidden="true"></i><p className={classes.text}>Music</p></NavLink>
+                    <NavLink to='/#' activeClassName={classes.active}><i className="fa fa-music" aria-hidden="true"></i><p className={classes.text}>Music</p></NavLink>
                 </div>
                 <div className={classes.item2 + ' ' + classes.settings}>
-                    <NavLink to='/#' activeClassName={classes.active}><i class="fa fa-cogs" aria-hidden="true"></i><p className={classes.text}>Settings</p></NavLink>
+                    <NavLink to='/#' activeClassName={classes.active}><i className="fa fa-cogs" aria-hidden="true"></i><p className={classes.text}>Settings</p></NavLink>
                 </div>
             </div>
             <hr></hr>
