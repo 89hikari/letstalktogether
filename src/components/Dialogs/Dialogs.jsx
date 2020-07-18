ac7baac8a4ee9e5ad36dabc11deae99add43fd9d
89hikari <vladislav0151@bk.ru>
2020-07-18 17:36:35 +0300

    styles

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import s from './Dialogs.module.css'
 import Message from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
+import { NavLink } from 'react-router-dom';
+import Name from './Name';
 
 const Dialogs = (props) => {
 
-    let dialogElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} ava={dialog.ava}/>);
+    let dialogElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} ava={dialog.ava} />);
     let messagesElements = props.state.messages.map(m => <Message message={m.message} />);
+    let separateElement = props.state.dialogs.map(elem => <Name name={elem.name} id={elem.id} />);
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                { dialogElements }
+                {dialogElements}
             </div>
             <div className={s.messages}>
-                { messagesElements }
+                <div className={s.chatHead}>
+                    {/*separateElement*/}
+                </div>
+                <div className={s.container}>
+                    {messagesElements}
+                </div>
             </div>
         </div>
     );
